e465944b1637395c5ae0ae9a87971e155cc5790e
Isak Larsson <55209313+IsakLarsson@users.noreply.github.com>
2021-05-27 14:41:11 +0200

    Update App.tsx

--- smart-shop/src/App.tsx
+++ smart-shop/src/App.tsx
@@ -14,9 +14,11 @@ interface Recipe {
 function App() {
   const [recipeList, setRecipeList] = useState<Recipe[]>([]);
   const [ingredientList, setIngredientList] = useState<Ingredient[]>([]);
-  const [ingredientName, setIngredient] = useState("");
+  const [ingredientName, setIngredientName] = useState("");
   const [recipeName, setRecipeName] = useState("");
   const [ingredientAmmount, setIngredientAmmount] = useState(0);
+  const [shoppingList, setShoppingList] = useState<Ingredient[]>();
+  const [chosenRecipes, setChosenRecipes] = useState<Recipe[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const value = event.target.value;
@@ -25,7 +27,7 @@ function App() {
         setRecipeName(value);
         break;
       case "ingredientName":
-        setIngredient(value);
+        setIngredientName(value);
         break;
 
       case "ingredientAmmount":
@@ -38,30 +40,102 @@ function App() {
   };
 
   const addIngredient = (): void => {
+    if (ingredientName === "" || ingredientAmmount === 0) return;
     const newIngredient: Ingredient = {
       name: ingredientName,
       ammount: ingredientAmmount,
     };
     const newList = [...ingredientList, newIngredient];
     setIngredientList(newList);
+    setIngredientName("");
+    setIngredientAmmount(0);
   };
 
   const addRecipe = (): void => {
+    if (recipeName === "") return;
     const newRecipe: Recipe = {
       name: recipeName,
       ingredients: ingredientList,
     };
     const newList = [...recipeList, newRecipe];
     setRecipeList(newList);
+    setIngredientList([]);
+  };
+
+  const handleRecipeClick = (index: number): void => {
+    const chosenRecipe = recipeList[index];
+    setChosenRecipes([...chosenRecipes, chosenRecipe]);
+  };
+
+  const createShoppingList = (): void => {
+    /**
+     * need to check if the ingredient exists since earlier, if it does just
+     * add the ammounts together. otherwise add the new ingredient
+     */
+    const r: Recipe[] = [
+      {
+        name: "köttfärs",
+        ingredients: [
+          { name: "vitlök", ammount: 1 },
+          { name: "gul lök", ammount: 1 },
+        ],
+      },
+      {
+        name: "good ole chicken",
+        ingredients: [
+          { name: "vitlök", ammount: 2 },
+          { name: "gains", ammount: 5 },
+        ],
+      },
+    ];
+    if (chosenRecipes.length === 0) return;
+    /* const res = r.reduce((acc, { ingredients }: Recipe) => {
+      ingredients.forEach((i) => {
+        const idx = acc.findIndex((ingredient) => ingredient.name === i.name);
+        if (!idx) acc[idx].ammount += i.ammount;
+        else acc.push({ ...i });
+      });
+      return acc;
+    }, []); */
+
+    chosenRecipes.forEach((recipe, index) => {
+      if (index === 0) {
+        setShoppingList(chosenRecipes[0].ingredients);
+        return;
+      }
+
+      /* ingredients.forEach((ingredient, index) => {
+         if (ingredient.name in newArr) {
+          newArr[ingredient.name] += ingredient.ammount;
+          console.log("exists");
+        } else newArr.push(ingredient); 
+      }); */
+    });
+  };
+
+  const add = (arr: Ingredient[], ingredient: Ingredient) => {
+    const { length } = arr;
+    const id = length + 1;
+    const ingredientName = ingredient.name;
+    const ingredientAmmount = ingredient.ammount;
+    const found = arr.some((ingredient) => ingredient.name === ingredientName);
+    if (!found) arr.push({ name: ingredientName, ammount: ingredientAmmount });
+    console.log(arr);
+  };
+
+  const ingredientExists = (ingredientName: string): boolean => {
+    return ingredientList.some(function (ingredient) {
+      return ingredient.name === ingredientName;
+    });
   };
 
   useEffect(() => {
-    console.log(
-      `RecipeName: ${recipeName}, ingredientName: ${ingredientName}, ingredientAmmount: ${ingredientAmmount}`
-    );
-    console.log(ingredientList);
+    console.log("ingredient list: ", ingredientList);
+
+    if (chosenRecipes.length > 0) createShoppingList();
+
     return () => {};
-  }, [ingredientList, recipeList]);
+  }, [ingredientList, recipeList, chosenRecipes]);
 
   return (
     <div className="App">
@@ -80,6 +154,7 @@ function App() {
           placeholder="Ingredient"
           id="ingredient"
           onChange={handleChange}
+          value={ingredientName}
         />
         <input
           type="number"
@@ -87,6 +162,7 @@ function App() {
           placeholder="Ammount"
           id="ingredientAmmount"
           onChange={handleChange}
+          value={ingredientAmmount}
         />
         <button onClick={addIngredient}>Add ingredient</button>
       </div>
@@ -112,7 +188,7 @@ function App() {
           {recipeList &&
             recipeList.map((recipe, index) => {
               return (
-                <li key={index}>
+                <li key={index} onClick={() => handleRecipeClick(index)}>
                   <h3>{recipe.name}</h3>
                   <p>
                     {recipe.ingredients &&
@@ -124,6 +200,9 @@ function App() {
               );
             })}
         </ul>
+        <div>
+          <h2>ShoppingList</h2>
+        </div>
       </div>
     </div>
   );
